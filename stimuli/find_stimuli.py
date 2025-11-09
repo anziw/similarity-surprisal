b7018385151c7b97f63867ce8b564c3ef768d869
Anzi Wang <anziwang@dhcp-10-103-232-187.wireless.northwestern.private>
2025-11-08 23:28:56 -0600

    update pipeline

--- stimuli/find_stimuli.py
+++ stimuli/find_stimuli.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import glob
 
+files = ["naturalstories/naturalstories_sentences_summary.csv", "provo/provo_sentences_summary.csv"]
 
-
-for file in glob.glob("*_summary.csv"):
+for file in files:
     df = pd.read_csv(file)
     df["Target"] = "0"
     df.loc[df["POS_Tags"].str.contains(r"\bJJ\s*,\s*JJ\b", regex=True), "Target"] = "JJJ"
     df.loc[df["POS_Tags"].str.contains(r"\bNN\s*,\s*NN\b", regex=True), "Target"] = "NNN"
-    df = df[df["Target"] != "0"]
-    out_file = file.replace("_sentences_summary.csv", "_stimuli.csv")
+    out_file = file.replace("_sentences_summary.csv", "_labeled.csv")
     df.to_csv(out_file, index=False)
