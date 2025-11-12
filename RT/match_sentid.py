bc5df13ba07436cf722b46b2b3b96b6517d5a921
Anzi Wang <anziwang@dhcp-10-103-232-187.wireless.northwestern.private>
2025-11-11 23:03:56 -0600

    sentid for rt files

--- RT/match_sentid.py
+++ RT/match_sentid.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_RT = pd.read_csv("naturalstories_RTs.csv")
+df_RT = pd.read_csv("processed_RTs.tsv", sep="\t")
 df_sent = pd.read_csv("naturalstories_sentences.csv")
 df_RT["Sent_ID"] = 0
 
@@ -11,7 +11,8 @@ for _, row in df_sent.iterrows():
     sent = row["Sent"]
     sent_id = row["Sent_ID"]
     sent_content = sent.split()
-    end += len(sent_content)
+    n = df_RT.iloc[start]['nItem']
+    end += len(sent_content) * n
     df_RT.loc[start:end-1, "Sent_ID"] = sent_id
     start = end
 
